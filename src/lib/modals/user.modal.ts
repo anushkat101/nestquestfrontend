f65cf82c85dbe8b826fee91bfbd5f590b5739ebe
anushkatrivedi1 <anushkat26@vt.edu>
2024-09-14 17:41:31 -0400

    schema changes

--- src/lib/modals/user.modal.ts
+++ src/lib/modals/user.modal.ts
@@ -1,35 +1,73 @@
-import {Schema, model, models} from "mongoose";
+import { Schema, model, models } from "mongoose";
+
+
+const JobOfferSchema = new Schema({
+   companyName: {
+       type: String,
+       required: true
+   },
+   location: {
+       type: String,
+       required: true
+   },
+   baseSalary: {
+       type: Number,
+       required: true
+   },
+   bonus: {
+       type: Number,
+       default: 0
+   },
+   signOnBonus: {
+       type: Number,
+       default: 0
+   },
+   relocationAmount: {
+       type: Number,
+       default: 0
+   },
+   otherExpenses: {
+       type: Number,
+       default: 0
+   },
+   equityDetails: {
+       type: String,
+       default: ''
+   },
+});
+
 
 const UserSchema = new Schema({
-    clerkID: {
-        type: String,
-        requied: true,
-        unique: true,
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: true,
-    },
-    username: 
-    {
-        type: String,
-        unique: true
-    }, 
-    photo:
-    {
-        type: String,
-        required: true,
-    },
-    firstname:
-    {
-        type: String,
-    },
-    lastName: {
-        type: String,
-    },
+   clerkID: {
+       type: String,
+       required: true,
+       unique: true,
+   },
+   email: {
+       type: String,
+       required: true,
+       unique: true,
+   },
+   username: {
+       type: String,
+       unique: true,
+   },
+   photo: {
+       type: String,
+       required: true,
+   },
+   firstname: {
+       type: String,
+   },
+   lastName: {
+       type: String,
+   },
+   jobOffers: [JobOfferSchema],
 });
+
 
 const User = models?.User || model("User", UserSchema);
 
+
 export default User;
+
